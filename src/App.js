33dd7fb2fd432e3495b180352e911956285f28f8
leolpaz <leonardolpaz95@gmail.com>
2022-02-17 12:10:31 -0300

    Add store setup and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import { useSelector } from 'react-redux';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 
 const App = () => {
+  const user = useSelector((state) => state.userReducer);
+
+  const checkUser = () => {
+    if (user !== "") {
+      return(
+        <>
+        <Route path="/addItem" element={'placeholder'} />
+        <Route path="/deleteItem" element={'placeholder'} />
+        <Route path="/displayItem" element={'placeholder'}>
+          <Route path=":id" element={'placeholder'} />
+        </Route>
+        </>
+      )
+    }
+    return <Route path="*" element={<Navigate to="/" />} />
+  }
+
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={'placeholderHome'} />
+        {checkUser()}
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/redux/user/user.js
@@ -0,0 +1,19 @@
+const ADD_USER = "redux-add-user"
+
+const initialState = ""
+
+export const addUser = (payload) => ({
+  type: ADD_USER,
+  payload,
+})
+
+const userReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_USER:
+      return action.payload;
+    default:
+      return state;
+  }
+}
+
+export default userReducer
