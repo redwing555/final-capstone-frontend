127944e900440fe037d92ef74a04dcf8fca641e5
leolpaz <leonardolpaz95@gmail.com>
2022-02-23 09:45:24 -0300

    Create the display items page

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -2,9 +2,11 @@ import { applyMiddleware, combineReducers, createStore } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import userReducer from './user/user';
+import carsReducer from './cars/cars';
 
 const reducer = combineReducers({
   userReducer,
+  carsReducer,
 });
 
 const store = createStore(reducer, applyMiddleware(thunk, logger));
